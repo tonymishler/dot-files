ae64f262a6a5c1ddc80592ced8997606148e406f
Tony Mishler <tony.mishler@gmail.com>
2022-10-04 08:24:41 -0500

    adding nerd tree commands

--- lua/mod2/remap.lua
+++ lua/mod2/remap.lua
@@ -1,5 +1,7 @@
 local nnoremap = require("mod2.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<leader>n", "<cmd>:NERDTreeFocus<CR>")
+nnoremap("<C-n>", "<cmd>:NERDTree<CR>")
 nnoremap("<leader>f", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>") 
 nnoremap("<leader>F", "<cmd>lua require'telescope.builtin'.live_grep{}<cr>") 
